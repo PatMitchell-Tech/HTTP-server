7b64756dbf915340a02a964d56d8c7f5f8546fa1
Patrick Mitchell <120431122+PatMitchell-Tech@users.noreply.github.com>
2024-04-26 18:02:01 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 from http.server import HTTPServer, CGIHTTPRequestHandler
 
-PORT = 80  #Choose your own port, just make sure not to choose a port that is currently running an active service
+class Simple_HTTPserver:
+    
+    def main():
+        PORT = 8000  #Choose your own port, just make sure not to choose a port that is currently running an active service
+        httpd = HTTPServer(("", PORT), CGIHTTPRequestHandler)
+        print(f"Your server is running on your localhost @: {PORT}")
+        httpd.serve_forever()#This runs the server
+    main()
 
-httpd = HTTPServer(("", PORT), CGIHTTPRequestHandler)
-print(f"Your server is running on your localhost @: {PORT}")
-httpd.serve_forever()#This runs the server
+    
